d430c993218125797de9d74f26232699ac85f9f1
Micael <micaelsilveira31@gmail.com>
2022-02-14 19:56:33 -0300

    update header.tsx, overlays buttons

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Header.module.css";
-import { BurgerSVG } from "../../Assets/IconsSVG";
+
 const Header: React.FC = () => {
   return (
     <div className={styles.container}>
@@ -8,7 +8,7 @@ const Header: React.FC = () => {
         <img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/742/6812533742_e1929b3c-4d04-4a32-8f7b-195fa85bcd48.png?cb=1644721829" />
       </div>
       <div className={styles.menu}>
-        <BurgerSVG />
+        <button className={styles.buttonMenu}>Menu</button>
       </div>
     </div>
   );
